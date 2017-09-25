e4497f5a6fe86fc2c99b6ff9ef5f68b8c72e0755
Sonny McPherson <unsungbomb@gmail.com>
2017-09-25 19:11:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(...chocolateBars, "milky Way") {
-
-  return chocolateBars
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array];
+  return newArray;
 }
 
-function destructivelyAddElementToBeginningOfArray(chocolateBars, chocolateBars.unshift("hersheys")){
-
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
 }
