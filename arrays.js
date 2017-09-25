2e9cebb95e3baf4841a57cf82516e216193f99fb
Sonny McPherson <unsungbomb@gmail.com>
2017-09-25 19:07:48 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(...chocolateBars, "milky Way") {
+
+  return chocolateBars
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, chocolateBars.unshift("hersheys")){
+  
+}
