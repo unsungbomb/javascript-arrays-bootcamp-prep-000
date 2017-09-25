67c287116119ded22876118740167e0eb7be51f1
Sonny McPherson <unsungbomb@gmail.com>
2017-09-25 19:16:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,13 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(array, element){
+  var newArray = [element, array...]
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+array.push(element)
+return array;
+}
